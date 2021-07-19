d3f8ef0849258739a81678efc45d93998f136967
Elliot Olbright <elliotolbright@gmail.com>
2021-07-19 15:38:11 -0600

    add application validations and tests

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord 
+  validates :name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :description, presence: true
+
   has_many :pet_applications, dependent: :destroy  
   has_many :pets, through: :pet_applications
 end 
